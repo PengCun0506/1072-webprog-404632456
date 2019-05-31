ee19555e1b46020e693d8374e12e53e649027cf1
PengCun0506 <wencun06@gmail.com>
2019-05-31 17:01:07 +0800

    w15

---
+++ w15/dbdemo-help/dbdemo2/routes/team.js
@@ -0,0 +1,5 @@
+const express = require('express');
+const router = express.Router();
+const teamController = require('../controllers/team');
+router.get('/', teamController.getTeam);
+module.exports = router;
